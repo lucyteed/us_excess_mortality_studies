17032806bc7aaa8969fef64449d09eb6b57138b4
lucyteed <122989093+lucyteed@users.noreply.github.com>
2023-09-03 11:19:13 -0400

    Add files via upload
    This R script includes the code for amalgamating the two weekly observed all-cause mortality counts from the Centers of Disease Control Prevention used in this research note.

---
+++ data_prep.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(lubridate)
+
+## TO DO: download datasets into home directory
+## https://data.cdc.gov/NCHS/Weekly-Counts-of-Deaths-by-State-and-Select-Causes/3yf8-kanr
+## https://data.cdc.gov/NCHS/Provisional-COVID-19-Death-Counts-by-Week-Ending-D/r8kw-7aab
+
+## The work in this research note accessed the second dataset on August 10, 2023.
+
+## read dataset for counts from January 2014-December 2019
+dt2014_2019 <- read.csv("~/Weekly-Counts-of-Deaths-by-State-and-Select-Causes.csv", 
+                        header=TRUE, sep=',')
+
+dt2014_2019 <- read.csv("/Users/lucyteed/weekly_2014_2019.csv",
+                        header=TRUE,sep=',')
+dt2020_present <- read.csv("/Users/lucyteed/cdc-data-final.csv",
+                           header=TRUE,sep=',')
+
+## read dataset for counts from January 2020-present
+dt2020_present <- read.csv("~/Provisional_COVID-19_Death_Counts_by_Week_Ending_Date_and_State.csv", 
+                           header=TRUE, sep=',')
+
+## change column names for consistency
+colnames(dt2014_2019)[1] <- "location"
+colnames(dt2014_2019)[2] <- "year"
+colnames(dt2014_2019)[3] <- "week"
+colnames(dt2014_2019)[4] <- "week_end"
+colnames(dt2014_2019)[5] <- "total_observed"
+
+colnames(dt2020_present)[3] <- "week_end"
+colnames(dt2020_present)[5] <- "year"
+colnames(dt2020_present)[7] <- "week"
+colnames(dt2020_present)[9] <- "location"
+colnames(dt2020_present)[11] <- "total_observed"
+
+## subset to weekly counts
+dt2020_present <- subset(dt2020_present, Group=="By Week")
+
+## only keep the necessary columns for all-cause excess mortality estimates
+dt2014_2019 <- dt2014_2019[-c(6:30)]
+dt2020_present <- dt2020_present[-c(1:2,4,6,8,10,12:17)]
+
+## subset to United States counts
+dt2014_2019 <- subset(dt2014_2019, location == "United States")
+dt2020_present <- subset(dt2020_present, location == "United States")
+
+## combine two datasets to gain data for January 2014-present
+data <- rbind(dt2014_2019, dt2020_present)
+
+## we only used data after 2015 until June 30
+data <- subset(data, year >= 2015)
+data <- subset(data, week_end < "07/01/2023")
+
+## save counts
+write.csv(data, "~/cdc-weekly11.csv", row.names=FALSE)
